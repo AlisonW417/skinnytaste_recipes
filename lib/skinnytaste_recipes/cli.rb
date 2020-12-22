52b4ca2561f25d5a3a2ba363f23beaa596f45b7e
Alison Williams <apw5011@gmail.com>
2020-12-22 01:47:29 +0000

    testing scraper

--- lib/skinnytaste_recipes/cli.rb
+++ lib/skinnytaste_recipes/cli.rb
@@ -1,5 +1,10 @@
 class SkinnytasteRecipes::CLI 
   def call 
     puts "Welcome to the Skinnytaste Recipe Selector!"
+    #display_categories
   end 
+  
+  #def display_categories 
+    #SkinnytasteRecipes::Scraper.get_categories
+  #end 
 end 
--- lib/skinnytaste_recipes/scraper.rb
+++ lib/skinnytaste_recipes/scraper.rb
@@ -1,7 +1,11 @@
 class SkinnytasteRecipes::Scraper
   def self.get_categories
     doc = Nokogiri::HTML(open("https://www.skinnytaste.com/recipe-index/"))
-    category = doc.css("div.recipe-cats div.item").text
+    #binding.pry 
+    category = doc.css("div.recipe-cats div.item")
+    category.each.with_index(1) do |cat, index|
+      puts "#{index}. #{cat.text}"
+    end 
   end 
   
 end 
